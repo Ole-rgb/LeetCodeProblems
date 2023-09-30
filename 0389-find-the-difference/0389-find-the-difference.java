510c3e72874988008c2a4a70cfa81adc9074082f
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2023-09-30 22:12:47 +0200

    Time: 10 ms (14.55%), Space: 41 MB (20.52%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+         Map<Character, Integer> count = new HashMap<>();
+        
+        for (char c : t.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : s.toCharArray()){
+            count.put(c, count.getOrDefault(c, 0) - 1);
+            if(count.get(c) == 0){
+                count.remove(c);
+            }
+        }
+
+
+        return (char) count.keySet().toArray()[0];        
+
+    }
+}
