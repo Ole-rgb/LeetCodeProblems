41dde20c08ff0db2945cc6ffd722ab57a5bc3712
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2023-10-07 01:33:19 +0200

    Time: 11 ms (29.26%), Space: 40.8 MB (17.65%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean isFascinating(int n) {
         String concatedNumbers = Integer.toString(n) + Integer.toString(2*n)+ Integer.toString(3*n);
         HashSet<Integer> numbers = new HashSet<>();
+    
+       if(concatedNumbers.contains("0")) return false;
+       if(concatedNumbers.length()>9) return false;
 
         for(char number : concatedNumbers.toCharArray()){
             if(number == '0'){
