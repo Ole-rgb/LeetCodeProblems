232eddd2eb3ead7a0a5f1821d8dc24ed2d3bd783
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2023-10-07 21:42:45 +0200

    Time: 1 ms (93.51%), Space: 41.3 MB (61.53%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -4,11 +4,12 @@ class Solution {
         int maxCandies = 0;
 
         //get the maxCandies
-        for(int candy : candies){
-            maxCandies = Math.max(maxCandies, candy);
-        }
+        for(int candy : candies) {
+            if(maxCandies<candy){
+                maxCandies=candy;
+            }
+      } 
 
-        //is candies[i] + extracCandies > maxCandies
         for(int candy : candies){
             greatesCandies.add(candy + extraCandies >= maxCandies);
         }
