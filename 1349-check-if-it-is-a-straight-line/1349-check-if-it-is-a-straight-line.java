0d9b6c65ae3c8180a64a554ef735f3ad27ad833d
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2023-10-10 16:38:29 +0200

    Time: 0 ms (100%), Space: 42.6 MB (84.34%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -13,19 +13,19 @@ class Solution {
             int xi=coordinates[i][0];
             int yi= coordinates[i][1];
  
-        /*
-            slope=(py2-py1)/(px2-x1) between the first two points 
-            compare that slope to other slopes (between point 0 and the ith point)
+            /*
+                slope=(py2-py1)/(px2-x1) between the first two points 
+                compare that slope to other slopes (between point 0 and the ith point)
 
-            (y2-y1)/(x2-x1) = (yi-py1)/(xi-x1)
-            
-            Possible error (xi-x1) == 0
+                (y2-y1)/(x2-x1) = (yi-py1)/(xi-x1)
+                
+                Possible error (xi-x1) == 0
 
-            therefor multiply by (x2-x1) and (xi-x1)
-            ->(y2-y1) * (xi-x1) = (yi-y1) * (x2-x1)
-            
-            indent indices
-        */
+                therefor multiply by (x2-x1) and (xi-x1)
+                ->(y2-y1) * (xi-x1) = (yi-y1) * (x2-x1)
+                
+                indent indices
+            */
         if((y1-y0)*(xi-x0) != (yi-y0)*(x1-x0)) return false;
         }
 
