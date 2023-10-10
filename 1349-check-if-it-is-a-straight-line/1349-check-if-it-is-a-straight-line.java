f8515a0219d6343e44d56adea70df29298d3e5fa
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2023-10-10 16:38:24 +0200

    Time: 0 ms (100%), Space: 42.4 MB (96.61%) - LeetHub

---
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+
+        if(coordinates.length<=2){
+            return true;
+        }
+        int x0= coordinates[0][0];
+        int y0= coordinates[0][1];
+        int x1=coordinates[1][0];
+        int y1= coordinates[1][1];
+
+        for(int i = 2; i<coordinates.length; i++){
+            int xi=coordinates[i][0];
+            int yi= coordinates[i][1];
+ 
+        /*
+            slope=(py2-py1)/(px2-x1) between the first two points 
+            compare that slope to other slopes (between point 0 and the ith point)
+
+            (y2-y1)/(x2-x1) = (yi-py1)/(xi-x1)
+            
+            Possible error (xi-x1) == 0
+
+            therefor multiply by (x2-x1) and (xi-x1)
+            ->(y2-y1) * (xi-x1) = (yi-y1) * (x2-x1)
+            
+            indent indices
+        */
+        if((y1-y0)*(xi-x0) != (yi-y0)*(x1-x0)) return false;
+        }
+
+        return true;
+    }
+
+}
