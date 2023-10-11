7814ffe3cf638b5fe681c3e9e75ce90bdfb9c9ff
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2023-10-12 00:13:37 +0200

    Time: 2 ms (39.4%), Space: 40.6 MB (91.47%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.java
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.java
@@ -6,8 +6,10 @@ class Solution {
     */  
     public int minOperations(List<Integer> nums, int k) {
         HashSet<Integer> set = new HashSet<>();
+        //i keeps track of the current element of the nums-list 
         int i=nums.size()-1;
         for(; i >= 0 && set.size() != k; i--){
+            //only add numbers that are smaller or equal to k 
             if(nums.get(i) <= k){
                 set.add(nums.get(i));
             }
