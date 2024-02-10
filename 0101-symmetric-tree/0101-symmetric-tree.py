baac2dde7fe676340af75e50f182edefeba86f33
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2024-02-11 00:45:28 +0100

    Time: 42 ms (33.49%), Space: 16.4 MB (92.01%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -12,8 +12,11 @@ class Solution:
         return self.__isMirror(root.left,root.right)
     
     def __isMirror(self, left, right)->bool:
+        #if both nodes are None, its symmetric
         if left == None and right == None:
             return True
+        #if one of the nodes is None, its not symmetric
         if left == None or right == None:
             return False
+        # compare the value of the given notes and recursively check the child nodes
         return left.val == right.val and self.__isMirror(left.left, right.right) and self.__isMirror(left.right, right.left) 
