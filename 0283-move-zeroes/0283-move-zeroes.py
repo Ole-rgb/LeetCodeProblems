3c2ea6386472196d4dda0b1e0af22dc4f1d6ebc0
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2024-02-11 14:28:05 +0100

    Time: 117 ms (95.48%), Space: 18.2 MB (52.95%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,36 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+
+        keep track of the current position(to check for zero) and the position of the next non zero 
+        -> two pointers
+
+        if nums[i] == 0
+        [0, 1, 0, 3, 12]
+        i^  
+        j^
+        
+        find first non zero to swap with and swap
+        [0, 1, 0, 3, 12]
+        i^  
+        j   ^
+        [1, 0, 0, 3, 12]
+        
+        [1, 0, 0, 3, 12]
+        i^  
+        j   ^        
+        """
+        i = 0
+        j = 1
+        while i < len(nums) and j < len(nums):
+            if nums[i] == 0:
+                #find next non zero then swap and increase indixes by one 
+                while j<len(nums)-1 and nums[j] ==0:
+                    j+=1
+                tmp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = tmp
+            i += 1
+            j += 1
+        
