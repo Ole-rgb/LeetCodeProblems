d20338d8a1fe621a1f9592578ce274981b8eea0d
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2024-02-17 19:43:45 +0100

    Time: 35 ms (98.16%), Space: 17.1 MB (65.45%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,19 +4,17 @@ class Solution:
             min=0
             max=len(row)-1
 
-            if  !(target >= row[min] and target <= row[max]):
-                continue
+            if target >= row[min] and target <= row[max]:
+                while min<=max:
+                    mid = (min+max)//2
 
-            while min<=max:
-                mid = (min+max)//2
-
-                if target < row[mid]:
-                    max = mid-1
-                    continue
-                if target > row[mid]:
-                    min = mid+1
-                    continue
-                
-                return True
+                    if target < row[mid]:
+                        max = mid-1
+                        continue
+                    if target > row[mid]:
+                        min = mid+1
+                        continue
+                    
+                    return True
 
         return False
