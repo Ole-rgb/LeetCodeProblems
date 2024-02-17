01648054870aab2bf945f01e7b4de7265649c206
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2024-02-17 19:40:07 +0100

    Time: 47 ms (55.76%), Space: 17.1 MB (87.07%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            min=0
+            max=len(row)-1
+
+            if target >= row[min] and target <= row[max]:
+                return self.number_in_row(row,target)
+
+        return False
+
+    def number_in_row(self, nums:List[int],target: int)->bool:
+        min = 0
+        max = len(nums)-1
+        
+        while min<=max:
+            mid = (min+max)//2
+
+            if target < nums[mid]:
+                max = mid-1
+                continue
+            if target > nums[mid]:
+                min = mid+1
+                continue
+            
+            return True
+
+        return False
