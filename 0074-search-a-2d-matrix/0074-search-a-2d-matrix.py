c6c3c3a27c5573c43d719682802925ef925eca25
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2024-02-17 19:40:56 +0100

    Time: 45 ms (67.51%), Space: 17.1 MB (87.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -8,31 +8,13 @@ class Solution:
                 while min<=max:
                     mid = (min+max)//2
 
-                    if target < nums[mid]:
+                    if target < row[mid]:
                         max = mid-1
                         continue
-                    if target > nums[mid]:
+                    if target > row[mid]:
                         min = mid+1
                         continue
                     
                     return True
-
-        return False
-
-    def number_in_row(self, nums:List[int],target: int)->bool:
-        min = 0
-        max = len(nums)-1
-        
-        while min<=max:
-            mid = (min+max)//2
-
-            if target < nums[mid]:
-                max = mid-1
-                continue
-            if target > nums[mid]:
-                min = mid+1
-                continue
-            
-            return True
 
         return False
