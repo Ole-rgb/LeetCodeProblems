e7fd50a5921afb4891240c2e8b028d15a5ce4033
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2025-02-04 23:11:46 +0100

    Time: 18 ms (13.38%), Space: 19.9 MB (12.43%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        #constant space??? -> remove pairs?->still = O(n/2)
-        #linear runtime -> one for loop over the array to find the number
         d = dict()
         for num in nums:
-            d[f"{num}"] = d.get(f"{num}", 0)+1
+            if d.get(f"{num}", None) is not None:
+                del  d[f"{num}"] 
+                continue
+
+            d[f"{num}"] = 1
         
-        for key, value in d.items():
-            if value == 1:
+        for key in d.keys():
                 return int(key)
