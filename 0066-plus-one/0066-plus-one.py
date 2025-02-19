94d54913d259c447d93d5cb5c7def5a534d37ff8
Ole Rößler <77236934+Ole-rgb@users.noreply.github.com>
2025-02-19 20:10:37 +0100

    Time: 0 ms (100%), Space: 17.9 MB (30.58%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,28 +1,21 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        #option one: read, then increment, then write
-        
-        #option two: increment in the array, without extracting the number 
-        #base case only increase lsb by one
-        #if lsb == 9 -> set to 0 and repeat for bit to the left
         num = self.parse_int(digits)
         res = self.write(num+1)
-        print(res)
         return res
         
 
     def parse_int(self, digits: List[int]) -> int:
         num = 0
-        for i in range(len(digits)):
+        for i in range(len(digits)): #O(n)
             num += digits[i] * (10**(len(digits)-i-1))
         return num
     
     def write(self, num: int) -> List[int]:
         l = list()
-        
-        while num > 0:
+        while num > 0: #O(n)
             l.append(num % 10)
             num //= 10
         
-        l.reverse()
+        l.reverse() #max O(n)
         return l
